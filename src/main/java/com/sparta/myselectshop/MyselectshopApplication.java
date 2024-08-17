65228e6e269fc5bf390f6de798e568cf09c52d1d
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-08-17 13:22:57 +0900

    카카오 사용자 정보로 회원가입 구현

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -3,11 +3,13 @@ package com.sparta.myselectshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MyselectshopApplication {
 
     public static void main(String[] args) {
